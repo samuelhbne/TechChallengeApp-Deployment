da3c60daf017255c70fce51567851a9bbe15ccf2
Samuel Huang <samuelh2006@gmail.com>
2021-02-13 19:16:37 +1100

    security_groups leads to force replacement always

--- aws/Bastion.tf
+++ aws/Bastion.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion1" {
   instance_type                 = var.instance_type_bastion
   subnet_id                     = aws_subnet.pub_subnet2.id
   associate_public_ip_address   = true
-  security_groups               = [aws_security_group.bastion_sg1.id]
+  vpc_security_group_ids        = [aws_security_group.bastion_sg1.id]
   tags = {
     Name = "${var.ENV}-bastion1"
   }
